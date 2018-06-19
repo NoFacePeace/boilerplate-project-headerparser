ee48d95ef26a9e3eab9901baaf3e6e174e2a6263
cht <736090467@qq.com>
2018-06-19 10:46:33 +0800

    fix: finish

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -25,6 +26,17 @@ app.get("/api/hello", function (req, res) {
 });
 
 
+app.get('/api/whoami', function(req, res) {
+    res.json({
+      ipaddress: req.ip,
+      language: req.headers['accept-language'],
+      software: req.headers['user-agent'],
+    });
+});
+
+app.use(function(req, res, next) {
+  res.status(404).type('text').send('Not Found');
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
